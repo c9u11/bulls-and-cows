da2290a28857aad053a252040166ba6f0fe46541
c9u11 <tjrdud6412@naver.com>
2022-02-08 17:44:06 +0900

    FEAT : Digit Component 수정
    - KeyDown을 인식하기 위해 onChange를 onKeyDown으로 변경
    - Input Tag Cursor 투명
    - focus Variant 추가 및 적용

--- src/pages/Test/components/Digit.tsx
+++ src/pages/Test/components/Digit.tsx
@@ -4,32 +4,42 @@ import styled from "styled-components";
 export interface DigitInterface {
   value: number;
   status: string;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onKeyDown?: React.KeyboardEventHandler<HTMLDivElement>;
 }
-export const Digit = ({ value, status, onChange }: DigitInterface) => {
-  const Digit = styled(motion.input)`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100px;
-    height: 130px;
-    background-color: rgba(255, 255, 255, 0.8);
-    border-radius: 5px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-    outline: none;
-    border: #777 3px solid;
-    text-align: center;
-    font-size: 64px;
-    font-weight: bolder;
-    color: black;
-    scale: 1;
-    rotateY: 0;
-    rotateZ: 0;
-  `;
+const DigitEl = styled(motion.input)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 130px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 5px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  outline: none;
+  border: #777 3px solid;
+  text-align: center;
+  font-size: 64px;
+  font-weight: bolder;
+  color: black;
+  scale: 1;
+  rotateY: 0;
+  rotateZ: 0;
+  caret-color: transparent;
+`;
+export const Digit = ({ value, status, onKeyDown }: DigitInterface) => {
   const digitVariants = {
     initial: {
-      color: "transparent",
-      borderColor: "white"
+      color: "rgba(255, 255, 255, 0.8)",
+      borderColor: "rgba(255,255,255)"
+    },
+    focus: {
+      borderColor: ["rgba(255, 255, 255, 0.8)", "#777"],
+      transition: {
+        repeat: Infinity,
+        repeatType: "reverse" as "reverse",
+        repeatDelay: 0,
+        duration: 1,
+      }
     },
     typed: {
       scale: [1, 1.13, 1],
@@ -89,13 +99,15 @@ export const Digit = ({ value, status, onChange }: DigitInterface) => {
     }
   };
   return (
-    <Digit
+    <DigitEl
       value={value}
-      disabled={!onChange}
-      onChange={!!onChange ? onChange : () => { }}
+      disabled={!onKeyDown}
+      onKeyDown={!!onKeyDown ? onKeyDown : () => { }}
+      onChange={() => { }}
       maxLength={1}
       variants={digitVariants}
       animate={status}
-    ></Digit>
+      whileFocus={"focus"}
+    ></DigitEl>
   )
 }
