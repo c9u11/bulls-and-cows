0e8880cb68faaaf8c93c77cc93585c5d95e10b7d
c9u11 <tjrdud6412@naver.com>
2022-02-13 22:54:56 +0900

    FEAT : ResultRow Component 추가
    결과를 보여주는 ResultRow Componenet 추가
    - animation이 다르고 결과를 수정하지 못하는 Component

---
+++ src/pages/Test/components/ResultRow.tsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { Digit } from "./Digit";
+
+export interface ResultRowInterface {
+  result: string;
+  answer: string;
+}
+
+const Wrapper = styled(motion.div)`
+  position: relative;
+  width: 100vw;
+  flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+const DigitWrapperVariants = {
+  result: {
+    transition: {
+      staggerChildren: 0.5
+    }
+  }
+}
+export const ResultRow = ({ result, answer }: ResultRowInterface) => {
+  return (
+    <Wrapper variants={DigitWrapperVariants} animate={"result"}>
+      {
+        result.split("").map((v, i) => {
+
+          return (
+            <Digit
+              key={v}
+              index={v}
+              status={v === answer[i] ? "full" : answer.indexOf(v) !== -1 ? "half" : "empty"}
+              value={v}
+              result={true}
+            />
+          )
+        })
+      }
+    </Wrapper>
+  );
+}
