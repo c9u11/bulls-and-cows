13e6da30c7b6da9a43a5443782c7b48117f4ad25
c9u11 <tjrdud6412@naver.com>
2022-02-13 22:55:41 +0900

    TEST : RowTestPage에서 4자리 테스트를 할 수 있도록 수정

--- src/pages/Test/Row/RowTestPage.tsx
+++ src/pages/Test/Row/RowTestPage.tsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { EditableRow } from '../components/EditableRow';
+import { ResultRow } from '../components/ResultRow';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 20vh;
+  min-height: 100vh;
+  background-color: ${props => props.theme.bgColor};
+  gap: 20px;
+`
+
+const answer = randomNum(4, true);
 
 export const RowTestPage = () => {
+  const [result, setResult] = useState<string[]>([]);
   return (
-    <EditableRow digitNum={4} unique={true} ></EditableRow>
+    <Container>
+      {
+        result.map((v, i) => (
+          <ResultRow key={`ResultRow${i}`} result={v} answer={answer}></ResultRow>
+        ))
+      }
+      {
+        result.indexOf(answer) === -1 ? <EditableRow digitNum={4} unique={true} setResult={setResult}></EditableRow> : null
+      }
+    </Container>
   );
+}
+
+function randomNum(digitNum: number, unique: boolean) {
+  const ouputArray: number[] = [];
+  function makeNum() {
+    if (ouputArray.length < digitNum) {
+      let n = Math.floor(Math.random() * 10);
+      if (!unique || notSame(n)) {
+        ouputArray.push(n);
+      }
+      makeNum();
+    }
+    function notSame(n: number) {
+      return ouputArray.every((e) => n !== e);
+    }
+  }
+  makeNum();
+  return ouputArray.join("");
 }
