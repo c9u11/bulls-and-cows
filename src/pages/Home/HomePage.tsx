db916a9582429ecf610cba065001cfe33ef3aa17
c9u11 <tjrdud6412@naver.com>
2022-03-17 22:12:05 +0900

    STYLE : ModalBtn 디자인 변경

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -5,13 +5,26 @@ import { modalAtom } from '../../atom/modalAtom';
 const Container = styled.div`
 `
 
+const ModalBtn = styled.button`
+  cursor: pointer;
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  background-color: ${props => props.theme.boxTextColor}33;
+  color: ${props => props.theme.boxTextColor};
+  border-radius: 50%;
+  border: ${props => props.theme.boxTextColor}11 3px solid;
+  width: 50px;
+  height: 50px;
+`
+
 export const HomePage = () => {
 
   const setModal = useSetRecoilState(modalAtom);
   return (
     <Container>
       <span>tes123t</span>
-      <button onClick={() => { setModal("test") }}>show modal</button>
+      <ModalBtn onClick={() => { setModal("test") }}>?</ModalBtn>
     </Container>
   )
 }
