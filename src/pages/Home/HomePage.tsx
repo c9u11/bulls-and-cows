8299f47f98336988ac2d486d7970b3b040614fbb
c9u11 <tjrdud6412@naver.com>
2022-04-12 10:22:06 +0900

    ADD : Fireworks Effect Test

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components"
 import { modalAtom } from '../../atom/modalAtom';
+import { Fireworks } from 'fireworks-js/dist/react'
 
 const Container = styled.div`
 `
@@ -22,10 +23,24 @@ const ModalBtn = styled.button`
 
 export const HomePage = () => {
 
+  const options = {
+    speed: 3
+  }
+
+  const style = {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    background: '#0000'
+  }
+
   const setModal = useSetRecoilState(modalAtom);
   return (
     <Container>
       <span>tes123t</span>
+      <Fireworks options={options} style={style as React.CSSProperties} />
       <ModalBtn onClick={() => { setModal("test") }}>Help</ModalBtn>
     </Container>
   )
