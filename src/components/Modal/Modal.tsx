65db80ef9a323a90485e7f5cbf2985d476ab9b04
c9u11 <tjrdud6412@naver.com>
2022-04-13 23:53:28 +0900

    STYLE : Modal 디자인 변경
    - Background Component 추가

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,13 +4,21 @@ import { useRecoilState } from "recoil";
 import { Test } from "./Test";
 import { Setting } from "./Setting";
 
-const Container = styled.div`
+const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
   position: fixed;
   top: 0px;
   z-index: 999;
   background: #00000088;
+`
+const Container = styled.div`
+  background-color: ${props => props.theme.boxBgColor};
+  color: ${props => props.theme.boxTextColor};
+  padding: 20px;
 `
 
 const returnModal = (modal: string) => {
@@ -24,7 +32,7 @@ export const Modal = () => {
   const [modal, setModal] = useRecoilState(modalAtom);
   const closeModal = () => { setModal("") }
   return (
-    <>
+    <Background>
       {
         modal
           ? <Container>
@@ -33,6 +41,6 @@ export const Modal = () => {
           </Container>
           : null
       }
-    </>
+    </Background>
   )
 }
