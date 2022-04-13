4cd169348716120e9433ec9b8c14f8601dc0090e
c9u11 <tjrdud6412@naver.com>
2022-04-14 00:24:38 +0900

    EDIT : FireWork Animation 위치 변경
    - Homapage > Test Modal
    - z-index -1 설정

--- src/components/Modal/Test.tsx
+++ src/components/Modal/Test.tsx
@@ -1,7 +1,26 @@
+import { Fireworks } from 'fireworks-js/dist/react';
+
 export const Test = () => {
+  const options = {
+    speed: 3
+  }
+
+  const style = {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    background: '#0000',
+    zIndex: -1,
+  }
+
   return (
-    <div>
-      asdfasdf
-    </div>
+    <>
+      <Fireworks options={options} style={style as React.CSSProperties} />
+      <div>
+        asdfasdf
+      </div>
+    </>
   )
 }
