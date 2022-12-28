978459f6301200aebd046531b6f14fa9f6d1ac87
c9u11 <tjrdud6412@naver.com>
2022-12-28 23:26:21 +0900

    ✏️ fileName
    - Theme > useTheme

--- src/components/BaseLayout/ThemeChanger.tsx
+++ src/components/BaseLayout/ThemeChanger.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { isDarkAtom } from "atom/themeAtoms";
 import { ReactComponent as LightIcon } from 'svg/light.svg';
 import { ReactComponent as DarkIcon } from 'svg/dark.svg';
-import { useTheme } from "hooks/Theme";
+import { useTheme } from "hooks/useTheme";
 
 const ChangeThemeBtn = styled.button`
   width: 50px;
