f4e9448921f54aa3a17b601f45b67d3e37d4a9af
c9u11 <tjrdud6412@naver.com>
2022-12-29 23:39:46 +0900

    ✨ setting w/ localstorage
    - constans
    - interfaces
    - init, get, set

---
+++ src/constants/Setting.ts
@@ -0,0 +1,6 @@
+export const LOCAL_STORAGE_KEY = "Setting";
+import { SettingInterface } from "../interfaces/Setting";
+
+export const DEFAULT_STATE: SettingInterface = {
+  darkMode: false,
+};
---
+++ src/interfaces/Setting.ts
@@ -0,0 +1,3 @@
+export interface SettingInterface {
+  darkMode: boolean;
+}
