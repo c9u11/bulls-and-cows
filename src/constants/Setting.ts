47bb6df64f8a12d16559241bd4d3bb774cd0ab44
c9u11 <tjrdud6412@naver.com>
2022-12-30 23:45:14 +0900

    :bug: import must be first

--- src/constants/Setting.ts
+++ src/constants/Setting.ts
@@ -1,5 +1,6 @@
-export const LOCAL_STORAGE_KEY = "Setting";
 import { SettingInterface } from "../interfaces/Setting";
+
+export const LOCAL_STORAGE_KEY = "Setting";
 
 export const DEFAULT_STATE: SettingInterface = {
   darkMode: false,
