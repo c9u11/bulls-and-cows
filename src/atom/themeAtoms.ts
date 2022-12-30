0f8e9ecf07fa65043de1388987cf14902894502a
c9u11 <tjrdud6412@naver.com>
2022-12-30 23:49:26 +0900

    ✨ Save/Load DarkMod
    - get darkmode
    - set darkmode

--- src/atom/themeAtoms.ts
+++ src/atom/themeAtoms.ts
@@ -1,7 +1,9 @@
 import { atom } from "recoil";
+import { getSetting } from "util/Setting";
 
+const { darkMode } = getSetting();
 
 export const isDarkAtom = atom({
   key: "isDark",
-  default: false,
-})
+  default: darkMode,
+});
--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,11 +1,17 @@
 import { isDarkAtom } from "atom/themeAtoms";
 import { useRecoilState, useSetRecoilState } from "recoil";
+import { getSetting, setSetting } from "util/Setting";
 
 export function useTheme() {
   const [isDark, setDark] = useRecoilState(isDarkAtom);
 
   const toggleDarkAtom = () => {
-    setDark((prev) => !prev);
+    setDark((prev) => {
+      const setting = getSetting();
+      setting.darkMode = !prev;
+      setSetting(setting);
+      return !prev;
+    });
   };
   return { isDark, toggleDarkAtom };
 }
