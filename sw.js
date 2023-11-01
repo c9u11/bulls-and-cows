e657f01c82772787df95a76c985deca91873eedd
c9u11 <tjrdud6412@naver.com>
2023-11-01 22:19:22 +0900

    Updates

--- sw.js
+++ sw.js
@@ -5,9 +5,8 @@ this.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
-        '/static/js/main.chunk.js',
-        '/static/js/0.chunk.js',
-        '/static/js/bundle.js',
+        '/static/js/main.d651aa26.js',
+        '/static/css/main.d19daf27.css',
         '/index.html',
         '/',
       ]);
